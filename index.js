25770d0485454f6fad4cd27b715e36842af35460
wento <florea_andreiwento@yahoo.com>
2023-02-22 00:50:13 -0600

    added index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//include packages needed for this application
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
